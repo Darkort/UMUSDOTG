64ecc79ec4ba8a30b122d39320dde904bc10e5af
Alesinx <alejandro7mr@gmail.com>
2019-03-31 11:48:31 +0200

    Deleter fixed

---
+++ UMUSDOTG6/Assets/Scripts/Deleter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deleter : MonoBehaviour
+{
+    bool debug = true;
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if (col.gameObject.tag == "asteroid") {
+            if (debug) Debug.Log("asteroid deleted");
+            Destroy(col.gameObject); 
+        }
+    }
+
+}
